b99e2f2d91cd58cbeca493aa0c27ca71503bc76f
tommyrharper <thomasrobertharper@gmail.com>
2021-03-12 03:20:12 +0000

    Create union-aliases.ts

---
+++ part2/union-aliases.ts
@@ -0,0 +1,28 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // }
+  // return result.toString();
+}
+
+const combinedAges = combine(30, 26, "as-number");
+const combinedAges2 = combine('30', '26', "as-number");
+const combinedNames = combine("Max", "Anna", "as-text");
+
+console.log(combinedAges);
+console.log(combinedAges2);
+console.log(combinedNames);
